333cb54b06c632af725b986ad8651f3f8e3a2973
wintersakura <darwinwoda@gmail.com>
2024-01-22 20:15:00 +0200

    fix: udpate git log alias with glg

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -16,7 +16,7 @@ alias gcm='git commit -m'
 alias ga='git add'
 alias gaa='git add --all'
 alias gst='git status'
-alias glog='git log --oneline --decorate --graph'
+alias glg='git log --oneline --decorate --graph'
 
 alias gco='git checkout'
 alias gcb='git checkout -b'

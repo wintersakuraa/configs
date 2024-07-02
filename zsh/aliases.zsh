94e64cf73efaa6a72d5c9616815ff4a028f1ae6f
wintersakura <darwinwoda@gmail.com>
2024-07-02 12:51:40 +0300

    [zsh] add git delete remote branch alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -69,4 +69,5 @@ alias grba='git rebase --abort'
 alias git-current-branch="git branch | grep \* | cut -d ' ' -f2"
 alias gpsup='git push --set-upstream origin $(git-current-branch)'
 alias gpf='git push --force'
+alias gpdo='git push -d origin'
 alias gp='git push'

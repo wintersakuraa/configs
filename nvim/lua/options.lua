5eb8332262d2510e6c24783154b5089d0b251346
wintersakura <darwinwoda@gmail.com>
2024-07-26 09:55:57 +0300

    [nvim] feat: add folding

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,7 +1,15 @@
-require "nvchad.options"
+require("nvchad.options")
 
 vim.opt.mouse = ""
 vim.opt.relativenumber = true
 vim.opt.undofile = false
 vim.opt.spell = true
 vim.opt.guicursor = ""
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "0"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldnestmax = 5
